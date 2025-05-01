7b00936c6b7474a3a3f846afa623213bd33f426a
320086703 <sibendu.das@philips.com>
2025-05-01 16:02:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react';        
+import React , { useState } from 'react';           
 
 import './static/vendor/bootstrap/css/bootstrap.min.css';
 import './static/vendor/bootstrap-icons/bootstrap-icons.css';
